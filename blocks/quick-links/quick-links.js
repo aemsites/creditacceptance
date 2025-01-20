6af3ba3ebbcbb60de9a55f9b9a808ef882ea5e84
kailasnadh790 <53608019+kailasnadh790@users.noreply.github.com>
2025-01-20 12:25:36 -0500

    Update quick-links.js

--- blocks/quick-links/quick-links.js
+++ blocks/quick-links/quick-links.js
@@ -1,11 +1,7 @@
-import { addStyles } from '../../libs/utils/utils.js';
-import { decorateIcons } from '../../scripts/aem.js';
-
 export default function decorate(block) {
   const nav = document.createElement('nav');
   const ul = document.createElement('ul');
   nav.append(ul);
-  decorateIcons(block);
 
   Array.from(block.children).forEach((row) => {
     const li = document.createElement('li');
@@ -32,24 +28,3 @@ export default function decorate(block) {
   block.innerHTML = '';
   block.append(nav);
 }
-
-// expose as a web component
-class AEMQuickLinksWebComponent extends HTMLElement {
-  // connect component
-  async connectedCallback() {
-    const shadow = this.attachShadow({ mode: 'open' });
-    const container = document.createElement('div');
-    container.className = 'quick-links';
-
-    // Define the slot
-    const slot = document.createElement('slot');
-    container.appendChild(slot);
-    await decorate(shadow);
-    shadow.prepend(addStyles('https://web-component--creditacceptance--aemsites.aem.page/blocks/quick-links/quick-links.css'));
-    shadow.prepend(addStyles('https://web-component--creditacceptance--aemsites.aem.page/styles/styles.css'));
-    shadow.prepend(addStyles('https://web-component--creditacceptance--aemsites.aem.page/styles/fonts.css'));
-  }
-}
-
-// register component
-customElements.define('aem-quick-links', AEMQuickLinksWebComponent);
