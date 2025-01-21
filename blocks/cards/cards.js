3cb8b61db5f200d87975befd7ccc6c2064b7cff1
Nicole Rego <nicole@thinkingbox.com>
2025-01-21 16:32:17 -0500

    [#13] Block: Cards - Resources, animation for shadow

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -1,10 +1,14 @@
 import { createOptimizedPicture } from '../../scripts/aem.js';
 
 export default function decorate(block) {
+  const isAnimated = block.classList.value.includes('animation');
   /* change to ul, li */
   const ul = document.createElement('ul');
   [...block.children].forEach((row) => {
     const li = document.createElement('li');
+    if (isAnimated) {
+      li.classList.add('animation-scale')
+    }
     while (row.firstElementChild) li.append(row.firstElementChild);
     [...li.children].forEach((div) => {
       if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
