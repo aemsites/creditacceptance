96f60073f58a1ece07999f2906639dd1f1a66992
kailasnadh790 <53608019+kailasnadh790@users.noreply.github.com>
2025-02-05 14:22:32 -0500

    Update jon-form.js

--- blocks/jon-form/jon-form.js
+++ blocks/jon-form/jon-form.js
@@ -1,10 +1,10 @@
 import { loadScript } from '../../scripts/aem.js';
 
-export default function decorate(block) {
+export default async function decorate(block) {
   window.jonEnv = 'test';
-  loadScript('/scripts/join-our-network-widget.js');
+  await loadScript('/scripts/join-our-network-widget.js');
   const formComponent = document.createElement('join-our-network');
-setTimeout(() => {
+  setTimeout(() => {
     block.appendChild(formComponent);
-}, 1000);
+  }, 1000);
 }
