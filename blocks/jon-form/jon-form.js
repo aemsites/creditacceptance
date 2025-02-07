5889a72e9c2ea386b6bea11496f6e5102192cdb1
kailasnadh790 <53608019+kailasnadh790@users.noreply.github.com>
2025-02-07 11:53:53 -0500

    fix

--- blocks/jon-form/jon-form.js
+++ blocks/jon-form/jon-form.js
@@ -1,6 +1,7 @@
 import { loadScript } from '../../scripts/aem.js';
 
 export default async function decorate(block) {
+  const SCRIPT_URL = 'https://s3.us-east-2.amazonaws.com/wwwbucket-join-network.teststatic.creditacceptance.com/join-our-network-widget.js ';
   if (window.location.host.endsWith('.live') || window.location.host.endsWith('creditacceptance.com')) {
     window.jonEnv = 'prod';
   } else {
@@ -8,19 +9,22 @@ export default async function decorate(block) {
   }
   const webContentJson = {};
   const rows = block.querySelectorAll('div > div');
+  let script = SCRIPT_URL;
 
   rows.forEach((row) => {
     const cells = row.querySelectorAll('div > p');
     if (cells.length === 2) {
       const key = cells[0]?.textContent?.trim();
       const value = cells[1]?.textContent?.trim();
-      if (key && value) {
+      if (key === 'script') {
+        script = value;
+      } else if (key && value) {
         webContentJson[key] = value;
       }
     }
   });
 
-  await loadScript('/scripts/join-our-network-widget.js');
+  await loadScript(script);
   const formComponent = document.createElement('join-our-network-form');
   formComponent.webContentJson = webContentJson;
   block.replaceChildren(formComponent);
