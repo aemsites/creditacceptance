96d81e19ec0dd326ff1b52fe9dcf4762b288fdfe
Ryan Parrish <churchofslidin@gmail.com>
2025-02-07 17:21:52 -0700

    Added initial section-grid support and a text-block to allow helper layouts etc.

---
+++ blocks/text/text.js
@@ -0,0 +1,10 @@
+import { decorateBlockBg } from '../../libs/utils/decorate.js';
+
+export default function decorate(block) {
+  block.classList.add('text-block'); // .text is too generic
+  const rows = block.querySelectorAll(':scope > div');
+  const foreground = rows[rows.length - 1];
+  const background = rows.length > 1 ? rows[0] : null;
+  if (background) decorateBlockBg(block, background, { useHandleFocalpoint: true });
+  foreground.classList.add('foreground');
+}
