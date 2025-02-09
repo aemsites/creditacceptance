e8bd863f1b209b5f9e4af0f5ff8d3ba6cef9497a
kailasnadh790 <53608019+kailasnadh790@users.noreply.github.com>
2025-02-09 17:08:25 -0500

    progress

--- blocks/carousel/carousel.js
+++ blocks/carousel/carousel.js
@@ -110,12 +110,22 @@ function createSlide(row, slideIndex, cId) {
   slide.dataset.slideIndex = slideIndex;
   slide.setAttribute('id', `carousel-${cId}-slide-${slideIndex}`);
   slide.classList.add('carousel-slide');
-
   row.querySelectorAll(':scope > div').forEach((column, colIdx) => {
     column.classList.add(`carousel-slide-${colIdx === 0 ? 'image' : 'content'}`);
     slide.append(column);
   });
 
+  const link = slide.querySelector('a');
+  if (link) {
+    const linkWrapper = document.createElement('a');
+    linkWrapper
+      .classList
+      .add('carousel-slide-link');
+    linkWrapper.setAttribute('href', link.getAttribute('href'));
+    linkWrapper.append(...slide.childNodes);
+    slide.append(linkWrapper);
+    link.remove();
+  }
   const labeledBy = slide.querySelector('h1, h2, h3, h4, h5, h6');
   if (labeledBy) {
     slide.setAttribute('aria-labelledby', labeledBy.getAttribute('id'));
---
+++ blocks/video-carousel/video-carousel.js
@@ -0,0 +1,52 @@
+import { buildBlock, loadBlock, loadCSS } from '../../scripts/aem.js';
+
+async function populateCarousel(videoLinks) {
+  const cells = [];
+  const promises = Array.from(videoLinks).map((link) => fetch(`https://vimeo.com/api/oembed.json?url=${link.href}`)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error('Failed to fetch metadata for video ID');
+      }
+      return response.json();
+    })
+    .then((metadata) => {
+      const cell = document.createElement('div');
+      const img = document.createElement('img');
+      img.src = metadata.thumbnail_url;
+      cell.append(img);
+      const h4 = document.createElement('h4');
+      h4.textContent = metadata.title;
+      cell.append(h4);
+      const a = document.createElement('a');
+      a.href = `#${metadata.video_id}`;
+      a.textContent = 'Watch Video';
+      cell.append(a);
+      cells.push([cell]);
+    })
+    .catch((error) => {
+      console.error(error);
+    }));
+
+  await Promise.all(promises);
+  console.log(cells);
+  const carousel = buildBlock('carousel', cells);
+  console.log(carousel);
+  carousel.dataset.blockName = 'carousel';
+  return loadBlock(carousel);
+}
+
+export default async function decorate(block) {
+  await loadCSS(`${window.hlx.codeBasePath}/blocks/embed/embed.css`);
+  block.classList.add('showcase-video');
+  const links = block.querySelectorAll('a');
+  block.innerHTML = '';
+  const firstVideo = links[0].cloneNode(true);
+  const embed = buildBlock('embed', { elems: [firstVideo] });
+  embed.dataset.blockName = 'embed';
+  const loadedEmbed = await loadBlock(embed);
+  block.append(loadedEmbed);
+  const carousel = await populateCarousel(links);
+  carousel.classList.add('slides-per-view-4');
+  carousel.classList.add('aspect-ratio-rectangle');
+  block.append(carousel);
+}
