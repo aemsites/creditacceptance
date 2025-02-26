7ce3a185771db03e711694fe59591ff04b6d5a96
Saugat Malla <saugat@thinkinbox.com>
2025-02-26 12:22:28 -0500

    nit

--- blocks/calculator/calculator-data-layer.js
+++ blocks/calculator/calculator-data-layer.js
@@ -20,9 +20,7 @@ export default function pushCalculatorDataLayer(block, fieldId) {
 
   const cacAnalytics = {
     is_spa: true,
-    tool_name: isMonthly ? 'monthly payment calculator' : 'auto affordability calculator',
     vehicle_type: block.querySelector('#type-of-auto').value,
-
     down_payment: parseFromString(block.querySelector('#down-payment').value),
     user_state: block.querySelector('#us-states').value,
     interest_rate: parseFromString(block.querySelector('#interest-rate').value),
@@ -33,9 +31,11 @@ export default function pushCalculatorDataLayer(block, fieldId) {
   };
 
   if (isMonthly) {
+    cacAnalytics.tool_name = 'monthly payment calculator';
     cacAnalytics.vehicle_price = parseFromString(block.querySelector('#auto-price').value);
     cacAnalytics.monthly_payment_estimate = parseFromString(block.querySelector('#estimated-monthly-price')?.textContent);
   } else {
+    cacAnalytics.tool_name = 'auto affordability calculator';
     cacAnalytics.preferred_monthly_payment = parseFromString(block.querySelector('#preferred-monthly-payment').value);
     cacAnalytics.auto_affordability_estimate = parseFromString(block.querySelector('#estimated-auto-price').textContent);
   }
