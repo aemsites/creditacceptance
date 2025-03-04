01cbb3bde1505254dccea86f2752f5e18a1214b5
Saugat Malla <saugat@thinkinbox.com>
2025-03-04 11:46:34 -0500

    remove intersection observer

--- blocks/jon-form/jon-form.js
+++ blocks/jon-form/jon-form.js
@@ -21,69 +21,50 @@ function loadScript(src, options) {
 }
 
 export default function decorate(block) {
-  const loadHeavyScripts = () => {
-    try {
-      const recaptchaScript = 'https://www.google.com/recaptcha/api.js';
-      let widgetScript = 'https://s3.us-east-2.amazonaws.com/wwwbucket-join-network.teststatic.creditacceptance.com/join-our-network-widget.js';
-      if (isProductionEnvironment()) {
-        widgetScript = 'https://wwwbucket-join-network.static.creditacceptance.com/join-our-network-widget.js';
-        window.jonEnv = 'prod';
-      } else {
-        window.jonEnv = 'test';
-      }
-
-      preloadScript(recaptchaScript);
-      preloadScript(widgetScript);
-
-      loadScript(recaptchaScript, { async: true })
-        .then(() => loadScript(widgetScript, { async: true }));
-
-      const webContentJson = {};
-      const rows = block.querySelectorAll('div > div');
-      rows.forEach((row) => {
-        const cells = row.querySelectorAll('div > p');
-        if (cells.length === 2) {
-          const key = cells[0]?.textContent?.trim();
-          const value = cells[1]?.textContent?.trim();
-          // If a new widget script is provided in the content, override it.
-          if (key === 'script') {
-            widgetScript = value;
-          } else if (key && value) {
-            webContentJson[key] = value;
-          }
-        }
-      });
-
-      block.innerHTML = '';
-      block.style.minHeight = '1000px';
-      block.style.position = 'relative';
-
-      const formComponent = document.createElement('join-our-network-form');
-      formComponent.webContentJson = webContentJson;
-      block.replaceChildren(formComponent);
-
-      formComponent.addEventListener('successData', () => {
-        window.location.href = '/dealers/join-our-network/confirmation-thank-you';
-      });
-    } catch (error) {
-      console.error('Error in decorate:', error);
+  try {
+    const recaptchaScript = 'https://www.google.com/recaptcha/api.js';
+    let widgetScript = 'https://s3.us-east-2.amazonaws.com/wwwbucket-join-network.teststatic.creditacceptance.com/join-our-network-widget.js';
+    if (isProductionEnvironment()) {
+      widgetScript = 'https://wwwbucket-join-network.static.creditacceptance.com/join-our-network-widget.js';
+      window.jonEnv = 'prod';
+    } else {
+      window.jonEnv = 'test';
     }
-  };
 
-  // fallback
-  if (!('IntersectionObserver' in window)) {
-    loadHeavyScripts();
-    return;
-  }
-
-  const observer = new IntersectionObserver((entries, observerInstance) => {
-    entries.forEach((entry) => {
-      if (entry.isIntersecting) {
-        loadHeavyScripts();
-        observerInstance.disconnect();
+    const webContentJson = {};
+    const rows = block.querySelectorAll('div > div');
+    rows.forEach((row) => {
+      const cells = row.querySelectorAll('div > p');
+      if (cells.length === 2) {
+        const key = cells[0]?.textContent?.trim();
+        const value = cells[1]?.textContent?.trim();
+        // If a new widget script is provided in the content, override it.
+        if (key === 'script') {
+          widgetScript = value;
+        } else if (key && value) {
+          webContentJson[key] = value;
+        }
       }
     });
-  });
 
-  observer.observe(block);
+    block.innerHTML = '';
+    block.style.minHeight = '1000px';
+    block.style.position = 'relative';
+
+    const formComponent = document.createElement('join-our-network-form');
+    formComponent.webContentJson = webContentJson;
+    block.replaceChildren(formComponent);
+
+    formComponent.addEventListener('successData', () => {
+      window.location.href = '/dealers/join-our-network/confirmation-thank-you';
+    });
+
+    preloadScript(recaptchaScript);
+    preloadScript(widgetScript);
+
+    loadScript(recaptchaScript, { async: true })
+      .then(() => loadScript(widgetScript, { async: true }));
+  } catch (error) {
+    console.error('Error in decorate:', error);
+  }
 }
