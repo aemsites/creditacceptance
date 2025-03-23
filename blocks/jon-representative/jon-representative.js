6ad96cceb613bc3ada006cd8a3bef0dde74782cc
Kailas Nadh U <53608019+kailasnadh790@users.noreply.github.com>
2025-03-22 21:18:58 -0400

    Jon representative (#530)
    * first commit
    
    * Update jon-form.js
    
    * Update jon-form.js
    
    * Update jon-form.js
    
    * Update jon-form.js
    
    * Update jon-form.css
    
    * changes
    
    * commit
    
    * fix
    
    * Update jon-form.css
    
    * fix
    
    * Update jon-form.css
    
    * Update jon-form.css
    
    * Update jon-form.css
    
    * style
    
    * Update jon-form.js
    
    * Update jon-form.css
    
    * Update jon-form.js
    
    * Update jon-form.js
    
    * lazy loading form
    
    * Update jon-form.css
    
    * Update jon-form.css
    
    * Update jon-form.js
    
    * Update jon-form.css
    
    * webworker
    
    * Update styles.css
    
    * Update related-cards.js
    
    * reverting worker
    
    * Update jon-form.js
    
    * Update jon-form.js
    
    * Update jon-form.js
    
    * Update jon-form.js
    
    * Update jon-form.js
    
    * Update blocks/jon-form/jon-form.css
    
    Co-authored-by: Ryan Parrish <churchofslidin@gmail.com>
    
    * first commit
    
    * rep
    
    * Update jon-representative.js
    
    * Update jon-representative.js
    
    ---------
    
    Co-authored-by: Ryan Parrish <churchofslidin@gmail.com>

---
+++ blocks/jon-representative/jon-representative.js
@@ -0,0 +1,54 @@
+/* eslint-disable max-len */
+import {
+  loadCSS, decorateIcons, buildBlock, loadBlock,
+} from '../../scripts/aem.js';
+import decorate from '../columns/columns.js';
+
+export default async function initialize(block) {
+  loadCSS(`${window.hlx.codeBasePath}/blocks/columns/columns.css`);
+
+  const jonRepresentativeData = localStorage.getItem('jon-representative');
+  if (jonRepresentativeData) {
+    try {
+      const jonRepresentative = JSON.parse(jonRepresentativeData);
+
+      if (jonRepresentative && jonRepresentative.name && jonRepresentative.title && jonRepresentative.phone && jonRepresentative.email) {
+        block.innerHTML = `
+            <div class="columns">
+              <div>
+                <div>
+                  <p><span class="icon icon-user-solid"></span><strong>${jonRepresentative.name}</strong></p>
+                  <p>${jonRepresentative.title}</p>
+                  <p><span class="border divider-thin-blue-dot"></span></p>
+                </div>
+              </div>
+              <div>
+                <div><p><span class="icon icon-phone-solid"></span><a href="tel:${jonRepresentative.phone}">${jonRepresentative.phone}</a></p>
+                <p><span class="border divider-thin-blue-dot"></span></p></div>
+              </div>
+              <div>
+                <div><p><span class="icon icon-envelope-solid"></span><a href="mailto:${jonRepresentative.email}">${jonRepresentative.email}</a></p>
+                <p><span class="border divider-thin-blue-dot"></span></p></div>
+              </div>
+            </div>
+        `;
+      }
+    } catch (error) {
+      console.error('Error parsing jon-representative data from localStorage:', error);
+    }
+    const columnsEl = block.querySelector('.columns');
+    if (!columnsEl) return;
+    const columns = buildBlock('columns', columnsEl);
+    columns.dataset.blockName = 'columns';
+    const loadedColumns = await loadBlock(columns);
+    block.innerHTML = loadedColumns.innerHTML;
+    const heading = document.createElement('h4');
+    heading.id = 'your-credit-acceptance-representative';
+    heading.textContent = 'Your Credit Acceptance representative:';
+    block.prepend(heading);
+
+    decorate(block.querySelector('.columns'));
+
+    decorateIcons(block);
+  }
+}
